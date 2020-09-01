4b7bedfb32f2e5b31edb4dcf997d2bb3bd8cef31
qiqi.huang <1090528892@qq.com>
2020-09-01 13:54:08 +0800

    添加测试用例

---
+++ spring-mock/src/main/java/com/demo/postProcessor/MyBeanPostProcessor.java
@@ -0,0 +1,20 @@
+package com.demo.postProcessor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class MyBeanPostProcessor implements BeanPostProcessor {
+
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("post Process Before Initialization is invoked");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("post Process after Initialization is invoked");
+        return bean;
+    }
+}
